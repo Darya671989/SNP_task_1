79760e7c62e24c92ce53e55cc3c5aff71d19d34f
Darya671989 <166034075+Darya671989@users.noreply.github.com>
2024-10-17 19:54:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const toggleHeaderScroll = () => {
+    const headerScroll = document.querySelector('.header-scroll')
+
+    window.addEventListener('scroll', (event) => {
+        if (window.scrollY >= 450) {
+            headerScroll.classList.add('animate')
+            
+        } else {
+            headerScroll.classList.remove('animate')
+        }
+    })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    toggleHeaderScroll()
+})     
+    
